ff4e67c76cb99050ed05ea135b9e41c65f1fe6e6
GWB <65540192+DFSgwb@users.noreply.github.com>
2022-05-27 17:34:53 +0800

    Create App.vue

---
+++ vue-cli_test/03_src_props配置/App.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+      <!---多个vc互不影响-->
+      <!--v-bind的作用重要 获取后面表达式值的赋给props的属性-->
+      <Student name="DFS" sex="男" :age='22'/>
+      <hr/>
+   </div>
+</template>
+
+<script>
+import Student from "./components/Student";
+
+export default {
+  name: "App",
+  components:{
+    Student,
+  },
+}
+</script>
